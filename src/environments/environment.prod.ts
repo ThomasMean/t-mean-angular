24fc5b7fe3a20c99d8bbc6f9935ea24e6cb17617
Tom Mean <mail@thomasmean.co.uk>
2019-10-05 23:02:44 +0100

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: 'AIzaSyBQRmedMllNh5aIgfCu8A_PxB19d7vUggE',
+    authDomain: 'tmeanwebsite.firebaseapp.com',
+    databaseURL: 'https://tmeanwebsite.firebaseio.com',
+    projectId: 'tmeanwebsite',
+    storageBucket: 'tmeanwebsite.appspot.com',
+    messagingSenderId: '651711277799',
+    appId: '1:651711277799:web:8eec6ce32ccdbdea68d042'
   }
 };
